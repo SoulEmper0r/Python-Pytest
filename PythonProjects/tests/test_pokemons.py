febef6efab955e898fa77e4271455277acd13f79
Александр <goroshekvbanochke@gmail.com>
2024-11-01 22:00:18 +0300

    Add files via upload

--- PythonProjects/tests/test_pokemons.py
+++ PythonProjects/tests/test_pokemons.py
@@ -7,15 +7,9 @@ HEADER = {'Content-Type': 'application/json',
           'trainer_token': TOKEN }
 trainer_id = '8280'
 
-def test_status_code():
-    response = requests.get(url= f'{URL}/pokemons')
+def test_trainers_status_code():
+    response = requests.get(url= f'{URL}/trainers')
     assert response.status_code == 200
-
-
-def test_trainer_id():
-    response = requests.get(url= f'{URL}/pokemons', params={'trainer_id': trainer_id, 'status':1})
-    Json_trainer_id = response.json()["data"][0]["trainer_id"]
-    assert Json_trainer_id == trainer_id
 
 def test_trainer_name():
     response = requests.get(url= f'{URL}/trainers', params={'trainer_id': trainer_id})
